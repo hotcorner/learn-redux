de29ff23d69664ac272164d49a842948501f0b3e
Clint Rowden <crowden@ea.com>
2019-01-15 16:00:36 -0500

    combined reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -4,6 +4,10 @@ import { routerReducer } from 'react-router-redux';
 import comments from './comments';
 import posts from './posts';
 
-const rootReducer = combineReducers({ posts, comments, router: routerReducer });
+const rootReducer = combineReducers({
+  posts,
+  comments,
+  routing: routerReducer
+});
 
 export default rootReducer;
