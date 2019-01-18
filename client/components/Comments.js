7c582c7adff75fbdb569444cbb2ff03b8f0f3c60
Clint <clintonia@gmail.com>
2019-01-18 18:45:57 -0500

    added eventhandlers, updated reducers to handle data and state

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -7,17 +7,38 @@ class Comments extends Component {
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comment">&times;</button>
+          <button
+            className="remove-comment"
+            onClick={this.deleteComment.bind(this, index)}
+          >
+            &times;
+          </button>
         </p>
       </div>
     );
   }
 
+  deleteComment(index) {
+    const { postId } = this.props.params;
+    this.props.deleteComment(postId, index);
+  }
+
+  handleComment(e) {
+    e.preventDefault();
+    const { postId } = this.props.params;
+    const { author, comment } = this.refs;
+    this.props.addComment(postId, author, comment);
+    e.target.reset();
+  }
   render() {
     return (
       <div className="comments">
-        {this.props.postComments.map(this.renderComment)}
-        <form ref="commentForm" className="comment-form">
+        {this.props.postComments.map(this.renderComment.bind(this))}
+        <form
+          ref="commentForm"
+          className="comment-form"
+          onSubmit={this.handleComment.bind(this)}
+        >
           <input type="text" ref="author" placeholder="Author" />
           <input type="text" ref="comment" placeholder="Your Comment" />
           <input type="submit" hidden />
--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,5 +1,24 @@
 function comments(state = [], action) {
+  if (typeof action.postId !== 'undefined') {
+    return {
+      ...state,
+      [action.postId]: postComments(state[action.postId], action)
+    };
+  }
   return state;
+}
+
+function postComments(state = [], action) {
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      const user = action.author.value;
+      const text = action.comment.value;
+      return [...state, { user, text }];
+    case 'DELETE_COMMENT':
+      return [...state.slice(0, action.i), ...state.slice(action.i + 1)];
+    default:
+      return state;
+  }
 }
 
 export default comments;
